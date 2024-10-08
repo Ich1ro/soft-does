20635063a81e8411d0586e0e380d0ec7ddc904b9
Taps73r <povroznykmisha@gmail.com>
2024-10-08 21:43:18 +0300

    Refactor: Сode cleanup and optimization in Insights page

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} ${jetbrainsMono.variable}`}>
+    <html lang="en" className={`dark ${inter.variable} ${jetbrainsMono.variable}`}>
       <body>{children}</body>
     </html>
   );
